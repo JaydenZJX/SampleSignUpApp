2d7af462b9d348e3ce0c8edf737cc7cb10e912fb
zxu <Jayden_Xu@intuit.com>
2021-04-24 22:09:07 -0700

    update btn color

--- app/src/main/java/com/example/simplesignup/view/ProfileCreationFragment.kt
+++ app/src/main/java/com/example/simplesignup/view/ProfileCreationFragment.kt
@@ -15,9 +15,7 @@ import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 /**
- * A simple [Fragment] subclass.
- * Use the [ProfileCreationFragment.newInstance] factory method to
- * create an instance of this fragment.
+ * Sample fragment to display profile creation input text ui
  */
 class ProfileCreationFragment : Fragment() {
 
